8a7e475844e5d52663230e35e8925892f2336b1e
Mikhail Nyagin <m.nyagin@yadro.com>
2025-09-28 17:00:26 +0300

    initial commit

---
+++ flat_service/src/test/java/soa/study/flat_service/FlatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package soa.study.flat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
